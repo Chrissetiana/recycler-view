e26982f5353c4a7e92220fed15fe230430c5a85d
chrissetiana <chrissetiana@gmail.com>
2018-08-07 01:01:41 +0100

    Add viewholder

--- app/src/main/java/com/chrissetiana/recyclerview/GreenAdapter.java
+++ app/src/main/java/com/chrissetiana/recyclerview/GreenAdapter.java
@@ -1,4 +1,58 @@
 package com.chrissetiana.recyclerview;
 
-public class GreenAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHolder> {
+
+    private static final String LOG_TAG = GreenAdapter.class.getSimpleName();
+
+    private int numItems;
+
+    GreenAdapter(int numOfItems) {
+        numItems = numOfItems;
+    }
+
+    @NonNull
+    @Override
+    public NumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.number_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+
+        return new NumberViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GreenAdapter.NumberViewHolder holder, int position) {
+        Log.d(LOG_TAG, "#" + position);
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return numItems;
+    }
+
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+        TextView listItemNumberView;
+
+        NumberViewHolder(View itemView) {
+            super(itemView);
+            listItemNumberView = itemView.findViewById(R.id.tv_item_number);
+        }
+
+        void bind(int listIndex) {
+            listItemNumberView.setText(String.valueOf(listIndex));
+        }
+    }
 }
--- app/src/main/java/com/chrissetiana/recyclerview/MainActivity.java
+++ app/src/main/java/com/chrissetiana/recyclerview/MainActivity.java
@@ -2,12 +2,28 @@ package com.chrissetiana.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_LIST_ITEMS = 100;
+    private GreenAdapter adapter;
+    private RecyclerView numberList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberList = findViewById(R.id.rv_numbers);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numberList.setLayoutManager(layoutManager);
+
+        numberList.setHasFixedSize(true);
+
+        adapter = new GreenAdapter(NUM_LIST_ITEMS);
+        numberList.setAdapter(adapter);
     }
 }
